9437e822ca54aa9fb0a5dd036c2ebf0b8afc32f8
sueKK <suzzykawira6@gmail.com>
2024-01-15 13:47:07 -0800

    pycache

---
+++ models/base_model.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+
+"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+
+    def _init_(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Converts the instance to a dictionary.
+
+        """
+        result_dict = self._dict_.copy()
+        result_dict['_class'] = self.__class.__name_
+        result_dict['created_at'] = self.created_at.isoformat()
+        result_dict['updated_at'] = self.updated_at.isoformat()
+        return result_dict
+
+    def _str_(self):
+        """
+        Returns a string representation of the instance.
+
+        """
+        return "[{}] ({}) {}".format(
+            self._class.__name, self.id, self.__dict_
+        )
