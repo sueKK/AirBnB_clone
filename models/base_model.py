29f72776d4414ff86fea5093d92a7705b98c1280
sueKK <suzzykawira6@gmail.com>
2024-01-17 14:08:02 -0800

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -8,10 +8,21 @@ from datetime import datetime
 
 class BaseModel:
 
-    def _init_(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def _init_(self, *args, **kwargs):
+        time_formart = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for key. value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_a" or key == "updated_at":
+                    setattr(self. key. datetime.strptime(value. time_format))
+                else:
+                    setattr(self. key. value)
+        else:
+            self.id = str(uuid.uuid4())
+
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         self.updated_at = datetime.now()
